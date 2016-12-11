ca2ca5b1fae524957a90cb780be34f21d9e4183e
unknown <alvi1005017@gmail.com>
2016-12-11 13:42:53 +0600

    Initial commit

---
+++ app/src/main/java/sudoku/alvi17/sudoku/MainActivity.java
@@ -0,0 +1,149 @@
+package sudoku.alvi17.sudoku;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        findViews();
+
+        setButtonListeners();
+
+		/*GradientDrawable gradient = new GradientDrawable(
+				Orientation.TL_BR,
+				new int[] {Color.argb(50, 251, 183, 166), Color.argb(50, 177, 239, 191)});*/
+
+        GradientDrawable gradient = new GradientDrawable(
+                GradientDrawable.Orientation.TL_BR,
+                new int[] {Color.argb(200, 147, 209, 131), Color.argb(200, 240, 182, 176)});
+
+        //MainActivity.this.getWindow().setBackgroundDrawable(gradient);
+
+
+        Drawable back = getResources().getDrawable(R.drawable.dada);
+
+        MainActivity.this.getWindow().setBackgroundDrawable(back);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, Settings.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                return false;
+        }
+    }
+
+    private void setButtonListeners() {
+        // TODO Auto-generated method stub
+        aboutButton.setOnClickListener(this);
+        newButton.setOnClickListener(this);
+
+        exitButton.setOnClickListener(this);
+
+        continueButton.setOnClickListener(this);
+    }
+
+    private void findViews() {
+        // TODO Auto-generated method stub
+        aboutButton = (Button) findViewById(R.id.about_button);
+        newButton = (Button) findViewById(R.id.new_button);
+
+        exitButton = (Button) findViewById(R.id.exit_button);
+
+        continueButton = (Button) findViewById(R.id.continue_button);
+
+    }
+
+    Button aboutButton = null;
+    Button newButton = null;
+    Button continueButton = null;
+    Button exitButton = null;
+    private static final String TAG = "MainActivity";
+
+
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.about_button:
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, About.class);
+                startActivity(intent);
+                break;
+
+            case R.id.new_button:
+                openNewGameDialog();
+                break;
+
+            case R.id.exit_button:
+                finish();
+                break;
+            case R.id.continue_button:
+                if (getSharedPreferences("PREF", MODE_PRIVATE)
+                        .getInt("flag", 3) != 3){
+                    Intent intentContinue = new Intent(MainActivity.this, Game.class);
+                    intentContinue.putExtra(Game.KEY_DIFFICULTY, Game.CONTINUE_KEY);
+                    startActivity(intentContinue);
+                }
+
+
+            default:
+                break;
+        }
+    }
+
+    private void openNewGameDialog() {
+        // TODO Auto-generated method stub
+		/*new AlertDialog.Builder(MainActivity.this).setTitle("��ʼ����Ϸ��������").setItems(
+				R.array.diffculty, new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						startGame(which);
+					}
+				}).show();*/
+
+        String which = Settings.getDiffState(getApplicationContext());
+        Integer i = Integer.parseInt(which);
+        startGame(i);
+
+    }
+
+    private void startGame(int i) {
+        Log.d(TAG, "click on " + i);
+        Intent intent = new Intent(MainActivity.this, Game.class);
+        intent.putExtra(Game.KEY_DIFFICULTY, i);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/sudoku/alvi17/sudoku/PuzzleView.java
@@ -0,0 +1,225 @@
+package sudoku.alvi17.sudoku;
+
+
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.FontMetrics;
+import android.graphics.Paint.Style;
+
+
+import android.graphics.Rect;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+public class PuzzleView extends View {
+
+	public PuzzleView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+
+		gameReference = (Game) context;
+		
+		
+
+		
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		// TODO Auto-generated method stub
+		super.onSizeChanged(w, h, oldw, oldh);
+
+		tileWidth = w / 9f;
+		tileHeight = h / 9f;
+
+		selRect = new Rect();
+
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		super.onTouchEvent(event);
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			int x = (int) (event.getX() / tileWidth);
+			int y = (int) (event.getY() / tileHeight);
+			select(x, y);
+			if (gameReference.getIsPenUsedFlag() || (gameReference.PEN_STATE_BEFORE_INT == 1)){
+				gameReference.setTestPen(x, y, 1);
+			}
+			gameReference.showKeypad(x, y);
+			break;
+
+		default:
+			break;
+		}
+
+		return true;
+	}
+
+	public void setSelectedTile(int i) {
+		gameReference.setTile(XOfSelection, YOfSelection, i);
+		invalidate();
+	}
+
+	private void select(int x, int y) {
+		XOfSelection = Math.min(Math.max(x, 0), 8);
+		YOfSelection = Math.min(Math.max(y, 0), 8);
+		getRect(XOfSelection, YOfSelection, selRect);
+		invalidate();
+	}
+
+	private void getRect(int x, int y, Rect rect) {
+		rect.set((int) (x * tileWidth), (int) (y * tileHeight), (int) (x
+				* tileWidth + tileWidth), (int) (y * tileHeight + tileHeight));
+
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+
+		super.onDraw(canvas);
+
+		drawBackground(canvas);
+		drawGrids(canvas);
+		drawNumbers(canvas);
+		drawFocusRect(canvas);
+		
+		judgeIfSucceed();
+
+	}
+
+	private void judgeIfSucceed() {
+		// TODO Auto-generated method stub
+		
+		if (gameReference.isSucceed()){
+			
+			new AlertDialog.Builder(gameReference)
+				.setTitle("congradulations")
+				.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+					
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						gameReference.finish();
+					}
+				})
+				.show();
+		}
+	}
+
+	private void drawFocusRect(Canvas canvas) {
+		// TODO Auto-generated method stub
+		Paint paintFocus = new Paint();
+		paintFocus.setColor(getResources().getColor(R.color.puzzle_selected));
+		canvas.drawRect(selRect, paintFocus);
+	}
+
+	public void drawNumbers(Canvas canvas) {
+		Paint numberPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		Paint preNumberPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		
+		
+		preNumberPaint.setColor(Color.BLUE);
+		numberPaint.setStyle(Style.FILL);
+		numberPaint.setTextSize(tileHeight * 0.75f);
+		preNumberPaint.setTextSize(tileHeight * 0.75f);
+		numberPaint.setTextScaleX(tileWidth / tileHeight);
+		preNumberPaint.setTextScaleX(tileWidth / tileHeight);
+		numberPaint.setTextAlign(Paint.Align.CENTER);
+		preNumberPaint.setTextAlign(Paint.Align.CENTER);
+		
+		
+
+		FontMetrics fm = numberPaint.getFontMetrics();
+		float x = tileWidth / 2;
+		float y = tileHeight / 2 - (fm.ascent + fm.descent) / 2;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				if (PuzzleView.this.gameReference.isInPreDefinedValues(j * 9
+						+ i)) {
+					canvas.drawText(PuzzleView.this.gameReference
+							.getTileString(i, j), i * tileWidth + x, j
+							* tileHeight + y, preNumberPaint);
+				} else {
+					/*
+					 * �����if-else��������else�е����ݷ���FontMetrics fm֮ǰ������bug���е��Ѷ��г��֣������
+					 */
+					//System.out.println(gameReference.getIsPenUsedFlag() && (gameReference.getTestPen(i, j) == 1));
+					//System.out.println(i + ", " + j + ": " + gameReference.getTestPen(i, j));
+					if(gameReference.getTestPen(i, j) == 1){
+						numberPaint.setColor(Color.RED);
+						
+					}else{
+						numberPaint
+						.setColor(getResources().getColor(R.color.puzzle_foreground));
+					}
+					
+					canvas.drawText(PuzzleView.this.gameReference
+							.getTileString(i, j), i * tileWidth + x, j
+							* tileHeight + y, numberPaint);
+				}
+
+			}
+		}
+	}
+
+	private void drawGrids(Canvas canvas) {
+		Paint darkPaint = new Paint();
+		darkPaint.setColor(getResources().getColor(R.color.puzzle_dark));
+		Paint hilitePaint = new Paint();
+		hilitePaint.setColor(getResources().getColor(R.color.puzzle_hilite));
+		Paint lightPaint = new Paint();
+		lightPaint.setColor(getResources().getColor(R.color.puzzle_light));
+
+		for (int i = 0; i < 9; i++) {
+			canvas.drawLine(0, i * tileHeight, getWidth(), i * tileHeight,
+					lightPaint);
+			canvas.drawLine(0, i * tileHeight + 1, getWidth(), i * tileHeight
+					+ 1, hilitePaint);
+			canvas.drawLine(i * tileWidth, 0, i * tileWidth, getHeight(),
+					lightPaint);
+			canvas.drawLine(i * tileWidth + 1, 0, i * tileWidth + 1,
+					getHeight(), hilitePaint);
+		}
+		for (int i = 0; i < 9; i++) {
+			if (i % 3 != 0)
+				continue;
+			canvas.drawLine(0, i * tileHeight, getWidth(), i * tileHeight,
+					darkPaint);
+			canvas.drawLine(0, i * tileHeight + 1, getWidth(), i * tileHeight
+					+ 1, hilitePaint);
+			canvas.drawLine(i * tileWidth, 0, i * tileWidth, getHeight(),
+					darkPaint);
+			canvas.drawLine(i * tileWidth + 1, 0, i * tileWidth + 1,
+					getHeight(), hilitePaint);
+		}
+	}
+
+	private void drawBackground(Canvas canvas) {
+		Paint backgroundPaint = new Paint();
+		backgroundPaint.setColor(getResources().getColor(
+				R.color.puzzle_background));
+		canvas.drawRect(0, 0, getWidth(), getHeight(), backgroundPaint);
+	}
+
+	private float tileWidth = 0f;
+	private float tileHeight = 0f;
+	private int XOfSelection = 0;
+	private int YOfSelection = 0;
+	private Game gameReference = null;
+	private Rect selRect = null;
+	
+	private static final String TAG = "PuzzleView.java";
+	
+	
+
+}
